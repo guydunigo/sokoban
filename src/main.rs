fd2f890c4ba6fd3777965e559f62746c577b5b3a
GONI Guillaume <gdn3@live.fr>
2024-09-30 22:53:03 +0200

    select other map file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 extern crate sokoban;
 
-use std::fs::read_to_string;
+use std::{env::args, fs::read_to_string};
 
-const LEVEL_FILENAME: &str = "./map.txt";
+const DEFAULT_LEVEL_FILENAME: &str = "./map.txt";
 
 fn main() {
-    let level = match read_to_string(LEVEL_FILENAME) {
+    let arg1 = args().nth(1);
+    let level_filename = arg1.as_ref().map_or(DEFAULT_LEVEL_FILENAME, |f| &f[..]);
+
+    let level = match read_to_string(level_filename) {
         Ok(l) => l,
         Err(err) => {
-            eprintln!("Could not open file `{}`: {}", LEVEL_FILENAME, err);
+            eprintln!("Could not open file `{}`: {}", level_filename, err);
             return;
         }
     };
